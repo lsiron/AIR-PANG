b584ebcedaa2cadf43ed62871dd27df12e3562a1
lsiron <l66612472@gmail.com>
2024-07-26 02:05:31 +0900

    초기구성 및 백,프론트 연결

---
+++ back/src/config/db.config.ts
@@ -0,0 +1,9 @@
+// db.config.ts
+// import mysql from 'mysql2/promise';
+
+// export const db = mysql.createPool({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'password',
+//   database: 'my_database',
+// });
