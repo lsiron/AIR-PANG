098ac5603bd6e70009c1f78005514ac133e7ed05
reamzitensi <guming0104@gmail.com>
2024-07-28 14:20:45 +0900

    feat: Google OAuth 인증 추가

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,13 +1,24 @@
 import express from 'express';
-import cors from 'cors';  // CORS 추가 프론트와의 연결을 위함
+import session from 'express-session';
+import cors from 'cors';
+import authRoutes from './routes/authRoutes';
 
 const app = express();
+const port = process.env.PORT || 5000;
 
-app.use(cors());  // CORS 미들웨어 추가
 app.use(express.json());
+app.use(cors());
+app.use(session({
+  secret: 'secret_key', // 실제 환경에서는 안전한 비밀 키를 사용하세요
+  resave: false,
+  saveUninitialized: true
+}));
 
-app.get('/', (req, res) => {
-  res.send('헬로월드!!');
+// 라우팅 설정
+app.use('/auth', authRoutes);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
 
-export { app };
+export default app;
---
+++ back/src/types/express-session.d.ts
@@ -0,0 +1,12 @@
+import 'express-session';
+
+declare module 'express-session' {
+  interface SessionData {
+    user?: {
+      sub: string;
+      name: string;
+      email: string;
+      picture: string;
+    };
+  }
+}
