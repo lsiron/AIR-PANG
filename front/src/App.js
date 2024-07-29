a64036830a644d29c5ad4c03c6178dc1245f8329
lsiron <l66612472@gmail.com>
2024-07-29 17:18:10 +0900

    style:geo 테스팅 리액트파일

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import HomePage from './HomePage';
+import LocationPage from './LocationPage';
+import Locations from './Locations';
+import LocationDetail from './LocationDetail';
+
 function App() {
   return (
-    <div>
-      <h1>Welcome back!</h1>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/locations">우리동네 찾기</Link>
+        </nav>
+        <h1>Welcome back!</h1>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/locations" element={<Locations />} />
+          <Route path="/location" element={<LocationPage />} />
+          <Route path="/location/detail" element={<LocationDetail />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
