7c11604a2dde5655dba71fee0fad114126daeadf
[AI11] 한정모 <hjm1044@naver.com>
2024-07-31 23:14:00 +0900

    feat: challenge 기능 구현

--- back/src/config/env.config.ts
+++ back/src/config/env.config.ts
@@ -1,4 +1,9 @@
-// env.config.ts
 import dotenv from 'dotenv';
+
+export const config = {
+    CLIENT_ID: process.env.GOOGLE_CLIENT_ID || '',
+    CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET || '',
+    REDIRECT_URI: process.env.GOOGLE_REDIRECT_URI || '',
+  };
 
 dotenv.config();
---
+++ front/src/App.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './styles/App.css';
+
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import LocationPage from './LocationPage';
+import Locations from './Locations';
+import LocationDetail from './LocationDetail';
+import ChallengeList from './components/ChallengeList';
+import ChallengeCreate from './components/ChallengeCreate';
+import ChallengeDetail from './components/ChallengeDetail';
+import ChallengeEdit from './components/ChallengeEdit';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/locations">우리동네 찾기</Link>
+          <Link to="/challenges">친환경 챌린지</Link>
+        </nav>
+        <Routes>
+          <Route path="/locations" element={<Locations />} />
+          <Route path="/location" element={<LocationPage />} />
+          <Route path="/location/detail" element={<LocationDetail />} />
+          <Route path="/challenges" element={<ChallengeList />} />
+          <Route path="/challenges/create" element={<ChallengeCreate />} />
+          <Route path="/challenges/:id" element={<ChallengeDetail />} />
+          <Route path="/challenges/edit/:id" element={<ChallengeEdit />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+
