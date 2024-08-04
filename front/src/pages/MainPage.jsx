d93cef7ffb94c6fd1a24c758de80617f4804f593
lsiron <l66612472@gmail.com>
2024-08-04 11:40:27 +0900

    프론트 병합

---
+++ front/src/pages/MainPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import WeatherLanding from "../components/Landing/WeatherLanding";
+function MainPage() {
+  return (
+    <div>
+      <WeatherLanding />
+    </div>
+  );
+}
+
+export default MainPage;
